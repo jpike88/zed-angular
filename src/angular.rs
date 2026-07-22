cec4f9a37f19eabfceb68b670d9bf81a3e054996
Josh Pike <josh@aquipa.com>
2026-07-22 17:20:05 +0700

    simplified version and removed installation action

--- src/angular.rs
+++ src/angular.rs
@@ -6,61 +6,32 @@ use zed::settings::LspSettings;
 use zed::CodeLabelSpan;
 use zed_extension_api::{self as zed, serde_json, Result};
 
-// The Latest version of typescript isn't always compatible with angular see: https://angular.dev/reference/versions#unsupported-angular-versions
-const DEFAULT_ANGULAR_LANGUAGE_SERVER_VERSION: &str = "21.1.5";
-const DEFAULT_TYPESCRIPT_VERSION: &str = "5.9.3";
-
 const SERVER_PATH: &str = "node_modules/@angular/language-server/index.js";
 const TYPESCRIPT_TSDK_PATH: &str = "node_modules/typescript/lib";
 
 const ANGULAR_LANGUAGE_SERVER_PACKAGE_NAME: &str = "@angular/language-server";
 const TYPESCRIPT_PACKAGE_NAME: &str = "typescript";
 
-#[derive(Deserialize, Default)]
-struct UserSettings {
-    angular_language_server_version: Option<String>,
-    typescript_version: Option<String>,
-}
-
 struct AngularExtension {
     did_find_server: bool,
-    angular_language_server_version: String,
-    typescript_version: String,
 }
 
 impl AngularExtension {
     #[allow(dead_code)]
     pub const LANGUAGE_SERVER_ID: &'static str = "angular";
 
-    fn read_user_settings(
-        &self,
-        language_server_name: &zed::LanguageServerId,
-        worktree: &zed::Worktree,
-    ) -> Result<UserSettings> {
-        let lsp_settings = LspSettings::for_worktree(&language_server_name.to_string(), worktree)?;
-
-        if let Some(options) = lsp_settings.initialization_options {
-            let user_settings: UserSettings = serde_json::from_value(options)
-                .map_err(|e| format!("Failed to parse initialization_options: {}", e))?;
-            Ok(user_settings)
-        } else {
-            Ok(UserSettings::default())
-        }
-    }
     fn file_exists_at_path(&self, path: &str) -> bool {
         fs::metadata(path).map_or(false, |stat| stat.is_file())
     }
 
     fn server_script_path(&mut self, language_server_id: &zed::LanguageServerId) -> Result<String> {
-        let server_exists = self.file_exists_at_path(&SERVER_PATH);
+        let server_exists = self.file_exists_at_path(SERVER_PATH);
 
         if self.did_find_server && server_exists {
             zed::set_language_server_installation_status(
                 language_server_id,
                 &zed::LanguageServerInstallationStatus::CheckingForUpdate,
             );
-
-            // TODO only install new version if there are change
         }
 
         zed::set_language_server_installation_status(
@@ -68,54 +39,16 @@ impl AngularExtension {
             &zed::LanguageServerInstallationStatus::Downloading,
         );
 
-        self.install_packages()?;
-
-        if !self.file_exists_at_path(&SERVER_PATH) {
+        if !self.file_exists_at_path(SERVER_PATH) {
             return Err(format!(
-                "Installed package '{}' did not contain expected path '{}'",
-                ANGULAR_LANGUAGE_SERVER_PACKAGE_NAME, SERVER_PATH
+                "Expected Angular language server path '{}' was not found in the project. Please ensure '{}' is installed in your project's node_modules.",
+                SERVER_PATH, ANGULAR_LANGUAGE_SERVER_PACKAGE_NAME
             )
             .into());
         }
 
         self.did_find_server = true;
         Ok(SERVER_PATH.to_string())
-    }
-
-    fn install_packages(&mut self) -> Result<()> {
-        let als_version = if self.angular_language_server_version == "latest" {
-            zed::npm_package_latest_version(ANGULAR_LANGUAGE_SERVER_PACKAGE_NAME)?
-        } else {
-            self.angular_language_server_version.clone()
-        };
-
-        let ts_version = if self.typescript_version == "latest" {
-            zed::npm_package_latest_version(TYPESCRIPT_PACKAGE_NAME)?
-        } else {
-            self.typescript_version.clone()
-        };
-
-        println!(
-            "Installing {}@{}, {}@{}",
-            ANGULAR_LANGUAGE_SERVER_PACKAGE_NAME, als_version, TYPESCRIPT_PACKAGE_NAME, ts_version
-        );
-
-        zed::npm_install_package(ANGULAR_LANGUAGE_SERVER_PACKAGE_NAME, &als_version).map_err(
-            |error| {
-                format!(
-                    "Failed to install package '{}': {}",
-                    ANGULAR_LANGUAGE_SERVER_PACKAGE_NAME, error
-                )
-            },
-        )?;
-        zed::npm_install_package(TYPESCRIPT_PACKAGE_NAME, &ts_version).map_err(|error| {
-            format!(
-                "Failed to install package '{}': {}",
-                TYPESCRIPT_PACKAGE_NAME, error
-            )
-        })?;
-
-        Ok(())
     }
 
     fn get_current_dir() -> Result<PathBuf> {
@@ -152,8 +85,6 @@ impl zed::Extension for AngularExtension {
     fn new() -> Self {
         Self {
             did_find_server: false,
-            angular_language_server_version: DEFAULT_ANGULAR_LANGUAGE_SERVER_VERSION.to_owned(),
-            typescript_version: DEFAULT_TYPESCRIPT_VERSION.to_owned(),
         }
     }
 
@@ -162,16 +93,6 @@ impl zed::Extension for AngularExtension {
         language_server_id: &zed::LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        let user_settings = self.read_user_settings(language_server_id, worktree)?;
-
-        if let Some(version) = user_settings.angular_language_server_version {
-            self.angular_language_server_version = version;
-        }
-
-        if let Some(version) = user_settings.typescript_version {
-            self.typescript_version = version;
-        }
-
         let server_path = self.server_script_path(language_server_id)?;
         let current_dir = env::current_dir().unwrap_or_else(|_| PathBuf::new());
         let full_path_to_server = current_dir.join(&server_path);
@@ -186,7 +107,7 @@ impl zed::Extension for AngularExtension {
         args.push("--ngProbeLocations".to_string());
         args.push(Self::get_ng_probe_locations(worktree).join(","));
 
-        // Provide the fallback SDK path inside the extension environment
+        // Provide the SDK path inside the project's folder hierarchy
         let absolute_tsdk_path = current_dir.join(TYPESCRIPT_TSDK_PATH);
         args.push("--tsdk".to_string());
         args.push(absolute_tsdk_path.to_string_lossy().to_string());
